6ae498f536b007cdde00aeb0770cf44151057526
Amonjot Singh Chhina <60524802+amonjotsingh@users.noreply.github.com>
2021-03-22 04:19:50 -0400

    Update ARDUINO_PROGRAM.ino

--- BEAGLEBONE/CONTROLLING_ARDUINO/USING_IRDA/ARDUINO_PROGRAM.ino
+++ BEAGLEBONE/CONTROLLING_ARDUINO/USING_IRDA/ARDUINO_PROGRAM.ino
@@ -1,22 +1,22 @@
-int mypin = 7;
-int value = 0;
+int mypin = 7; //declaring GPIO pin number that would be used
+int value = 0; //declaring variable and intializes
 
 void setup() {
-Serial.begin(9600);
-pinMode(mypin,INPUT);
+Serial.begin(9600);   //setting up baud rate
+pinMode(mypin,INPUT);   //setting up input GPIO pin
 
 }
 
 // This function will loop as quickly as possible, forever.
 void loop() 
 {
-  delay(500);
-  value = digitalRead(mypin);
-  Serial.println(value);
-  if(value == 0){
+  delay(500);     //delay of 500 ms
+  value = digitalRead(mypin);    //reading input gpio pin and passing the value to value variable
+  Serial.println(value);      //printing value of value
+  if(value == 0){             //checking value and writing
     digitalWrite(13, LOW);
   }
-  if(value == 1){
+  if(value == 1){               
     digitalWrite(13,HIGH);
   }
 }
