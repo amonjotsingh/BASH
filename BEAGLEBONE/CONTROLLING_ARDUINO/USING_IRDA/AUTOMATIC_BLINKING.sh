ea5c7e8c05334bb31195d4440ca75a48a9b3e5e0
Amonjot Singh Chhina <60524802+amonjotsingh@users.noreply.github.com>
2021-03-22 04:20:28 -0400

    Update AUTOMATIC_BLINKING.sh

--- BEAGLEBONE/CONTROLLING_ARDUINO/USING_IRDA/AUTOMATIC_BLINKING.sh
+++ BEAGLEBONE/CONTROLLING_ARDUINO/USING_IRDA/AUTOMATIC_BLINKING.sh
@@ -1,7 +1,7 @@
 ##In this program we are toggling the output between 0 and 1 after 0.5 second delay 
-
+#changing directory
 cd /sys/class/gpio/gpio45
-
+#infinite loop
 while [ 1 -eq 1 ]
 do
         echo 0 > value 
